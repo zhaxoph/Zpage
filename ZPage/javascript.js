ad1203e2c62d6981f45be2aa5ef9c25033cecc17
Arturo Martinez <zloaded@gmail.com>
2018-10-30 18:58:52 -0300

    hash recognition updates

--- ZPage/javascript.js
+++ ZPage/javascript.js
@@ -1,10 +1,7 @@
 window.onload = function(e){
-  console.log("welcome", document.getElementById("welcome"));
 // get nav links
 var links = document.querySelectorAll("sidenav a"); 
 var links = document.querySelectorAll("nav a"); 
-var hash = location.hash;
-var hash = location.hash.substr(1);
 // iterate through links and assign on click action
 links.forEach(function(link){
 link.addEventListener("click", onLinkClick); 
@@ -13,8 +10,6 @@ link.addEventListener("click", onLinkClick);
 function onLinkClick(e){
   // prevent default behavior so that it doesn't scroll instantly based on hash
   //e.preventDefault();
-
-  
   links.forEach(function(link){
     if(link != e.target && window.location.hash){
       link.classList.remove("current"); 
@@ -27,39 +22,41 @@ function onLinkClick(e){
 window.onscroll = function(e){
   // Getting the nav offsetTop element
   var d = document.getElementById("nav");
-  // Assigning the value of offsetTop
-  var topPos = d.offsetTop;  
-  var welcomePosition = document.getElementById("welcome").offsetHeight;
-  var y = window.scroll;
+  // Assigning the value of offsetHeight with section welcome 
+  var welcomePosition = document.getElementById("welcome").offsetTop;
+  var myworkPosition = document.getElementById("my-work").offsetTop;
+  var aboutPosition = document.getElementById("about").offsetTop;
+
+  var currentSection = '';
+  
  //When the topnav is offsetTop, Sidenav will fade in
- if (window.scrollY > welcomePosition){
-   console.log(window.location.hash);
-   
-  document.getElementById("sidenav").classList.add("nav-active");   
- } else{
+ if (window.scrollY > aboutPosition){
+  if(currentSection !== 'aboutPosition' && history.pushState) {
+    history.pushState(null, null, '#about');
+    currentSection = 'aboutPosition';
+  }
+     
+  } else if (window.scrollY > myworkPosition ) {
+    if(currentSection !== 'myworkPosition' && history.pushState) {
+      history.pushState(null, null, '#my-work');
+      currentSection = 'myworkPosition';
+    }
+  } else if (window.scrollY > welcomePosition){
+    if(currentSection !== 'welcomePosition' && history.pushState) {
+      history.pushState(null, null, '#welcome');
+      currentSection = 'welcomePosition';
+    }
+    document.getElementById("sidenav").classList.add("nav-active");
+  }else{
   document.getElementById("sidenav").classList.remove("nav-active");
  } 
 };
 
 
 
-/*
-window.onscroll = function() {scrollAppear()};
-// Get the navbar
-var navbar = document.getElementById("sidenav");
 
-// Get the offset position of the navbar
-var sticky = navbar.addEventListener("scroll", scrollAppear);
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function scrollAppear() {
-  if (window.pageYOffset >= sticky) {
-    sidenav.classList.add("vis")
-  } else {
-    sidenav.classList.remove("vis");
-  }
-}
-*/
+
 /*
 var slideIndex = 1;
     showSlides(slideIndex);
