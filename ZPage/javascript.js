de570eca09a2ee300c3ec9cd607cbb0f40500c87
Arturo Martinez <zloaded@gmail.com>
2018-11-01 01:26:37 -0300

    Throttling eliminated. Bug fix on scroll

--- ZPage/javascript.js
+++ ZPage/javascript.js
@@ -5,8 +5,17 @@ var links = document.querySelectorAll("nav a");
 // iterate through links and assign on click action
 links.forEach(function(link){
 link.addEventListener("click", onLinkClick); 
+link.addEventListener("scroll", onLinkScroll)
 });
-
+function onLinkScroll(e){
+  console.log("hola");
+  links.forEach(function(link){
+    if (link != e.target && window.location.hash){
+      link.classList.remove("current");
+    }
+  });
+  e.target.classList.add("current");
+}
 function onLinkClick(e){
   // prevent default behavior so that it doesn't scroll instantly based on hash
   //e.preventDefault();
@@ -18,33 +27,39 @@ function onLinkClick(e){
   e.target.classList.add("current");
   }
 }
+var welcome = document.getElementById("welcome");
+var myWork = document.getElementById("my-work");
+var about = document.getElementById("about");
+var currentSection = '';
 
 window.onscroll = function(e){
   // Getting the nav offsetTop element
   var d = document.getElementById("nav");
   // Assigning the value of offsetHeight with section welcome 
-  var welcomePosition = document.getElementById("welcome").offsetTop;
-  var myworkPosition = document.getElementById("my-work").offsetTop;
-  var aboutPosition = document.getElementById("about").offsetTop;
+  var welcomePosition = welcome.offsetTop;
+  var myworkPosition = myWork.offsetTop;
+  var aboutPosition = about.offsetTop;
 
-  var currentSection = '';
+  
   
  //When the topnav is offsetTop, Sidenav will fade in
- if (window.scrollY > aboutPosition){
-  if(currentSection !== 'aboutPosition' && history.pushState) {
-    history.pushState(null, null, '#about');
-    currentSection = 'aboutPosition';
-  }
-     
+  if (window.scrollY > aboutPosition){
+    if(currentSection !== 'aboutPosition' && history.pushState) {
+      history.pushState(null, null, '#about');
+      currentSection = 'aboutPosition';
+      
+    }
   } else if (window.scrollY > myworkPosition ) {
     if(currentSection !== 'myworkPosition' && history.pushState) {
       history.pushState(null, null, '#my-work');
       currentSection = 'myworkPosition';
+      
     }
   } else if (window.scrollY > welcomePosition){
     if(currentSection !== 'welcomePosition' && history.pushState) {
       history.pushState(null, null, '#welcome');
       currentSection = 'welcomePosition';
+      
     }
     document.getElementById("sidenav").classList.add("nav-active");
   }else{
